211a36849282603cd17ee5293a8da44019f485ea
tuantm0303 <minhtuan0330@gmail.com>
2023-07-18 23:52:34 +0700

    db onl

--- src/connections/mongo.js
+++ src/connections/mongo.js
@@ -7,7 +7,8 @@ mongoose.set("strictQuery", true);
 
 export const connectionMongo = mongoose
   .connect(
-    `${runLocalhost.uri}:${runLocalhost.port}/${runLocalhost.databaseName}`
+    // `${runLocalhost.uri}:${runLocalhost.port}/${runLocalhost.databaseName}`
+    'mongodb+srv://minhtuan0330:Xlb1PGWwqcbT7vmY@cluster0.1jvxnvh.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(() => console.log("Connect Database success!"))
   .catch((error) => console.log('error: ', error));
